4877ce85f0b1e6da8815ae89717beddf1cc460e8
Bates Jernigan <bates1012@aol.com>
2012-05-18 09:22:52 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Funstation::Application.config.secret_token = 'e74030e4c1147af3f02ef222d3ea89e3dc71d69034b8367041548a155ad6983c8a4528f37a34d9943772af437e5461a749443868cf41994ba61e5d62e02e176f'
